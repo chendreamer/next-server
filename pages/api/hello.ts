700a2180f039977fd3607327877838ea63d5361b
chenshuaiyu <1961569938@qq.com>
2022-05-05 18:20:03 +0800

    完善入门项目

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { getAllPostIds } from '../../lib/posts'
 
 type Data = {
-  name: string
+    name: string
 }
 
 export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+    console.log(req);
+    const paths = getAllPostIds()
+    paths.push({ name: 'jack jones' });
+    //res.status(200).json({ name: 'John Doe999' })
+    res.status(200).json(paths);
 }
---
+++ pages/posts/[id].js
@@ -0,0 +1,36 @@
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+
+export default function Post({ postData }) {
+    return (
+        <Layout>
+            {postData.title}
+            <br />
+            {postData.id}
+            <br />
+            {postData.date}
+            <br />
+            22
+            <br />
+            {postData.content}
+        </Layout>
+    )
+}
+
+export async function getStaticProps({ params }) {
+    const postData = getPostData(params.id)
+    console.log(postData);
+    return {
+      props: {
+        postData
+      }
+    }
+  }
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds()
+    return {
+        paths,
+        fallback: false
+    }
+}
